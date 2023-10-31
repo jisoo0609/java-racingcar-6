2868a156a6ee307088a04746402536a608e8e6c3
jisoo0609 <jisu20000609@gmail.com>
2023-10-31 16:25:39 +0900

    no message

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,23 +1,28 @@
 package racingcar;
 
-import racingcar.controller.Score;
-import racingcar.controller.Winner;
+import racingcar.controller.TotalScore;
 import racingcar.model.Car;
+import racingcar.model.TryCount;
+import racingcar.view.ResultView;
 
 import java.util.List;
-import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Car car = new Car("pobi,woni,jun");
+        List<String> carNames = car.input();
 
-        Car car = new Car();
-        List<String> carName = car.input();
+        TryCount tryCount = new TryCount();
+        int count = tryCount.count();
 
-        Winner winner = new Winner();
-        List<Integer> point = winner.playerScore(carName);
+        for (int i=0; i<count; i++) {
+            for (String name : carNames) {
+                car = new Car(name);
+                car.CarPlay();
+            }
+            System.out.println();
 
-        System.out.println(point);
-
+        }
     }
 }
