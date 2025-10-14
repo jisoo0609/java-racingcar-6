e00ad06ca8d0d054f591663d7029198c9cdf4e27
JISOO <jisu20000609@gmail.com>
2025-10-14 20:06:24 +0900

    feat: RacingGame.java validate

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -8,10 +8,24 @@ public class RacingGame {
     private final int tryConut;
 
     public RacingGame(List<Car> cars, int tryConut) {
-        // 검증 validate
+        validateCars(cars);
+        validateTryCount(tryConut);
 
         this.cars = cars;
         this.tryConut = tryConut;
     }
+
+    private void validateCars(List<Car> cars) {
+        if (cars.isEmpty()) {
+            throw new IllegalArgumentException("자동차가 최소 1대 이상이어야 합니다.");
+        }
+    }
+
+    private void validateTryCount(int tryConut) {
+        if (tryConut <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 1번 이상이여야 합니다.");
+        }
+    }
+
 
 }
