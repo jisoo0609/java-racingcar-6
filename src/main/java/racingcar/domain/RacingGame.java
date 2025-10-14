7a1df2e332a998636b003d347ea10e0b20c20547
JISOO <jisu20000609@gmail.com>
2025-10-14 20:24:23 +0900

    feat: playRound()

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import racingcar.util.RandomUtils;
+
 import java.util.List;
 
 // 자동차들을 관리하고 라운드 실행
@@ -42,7 +44,10 @@ public class RacingGame {
 
     // 한 라운드 실행
     public void playRound() {
-
+        for (Car car : cars) {
+            int randomNumber = RandomUtils.getRandomNumber();
+            car.move(randomNumber);
+        }
     }
 
 /*
