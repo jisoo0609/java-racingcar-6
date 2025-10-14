8f3ad935351796f216f09a09baafea0d36e2b615
jisoo0609 <jisu20000609@gmail.com>
2025-10-14 18:49:51 +0900

    feat: structure

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -4,5 +4,14 @@ import java.util.List;
 
 // 자동차들을 관리하고 라운드 실행
 public class RacingGame {
+    private final List<Car> cars;
+    private final int tryConut;
+
+    public RacingGame(List<Car> cars, int tryConut) {
+        // 검증 validate
+
+        this.cars = cars;
+        this.tryConut = tryConut;
+    }
 
 }
