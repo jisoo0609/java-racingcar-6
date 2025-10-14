56eb1e442c8ceb6ef30da8fc41fe7c027619c5de
JISOO <jisu20000609@gmail.com>
2025-10-14 21:09:53 +0900

    refactor: Application.java

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,9 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 1. 자동차 이름 입력
+        // 2. 시도 횟수 입력
+        // 3. 게임 실행
+        // 4. 최종 우승자 출력
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++
@@ -1,5 +0,0 @@
-package racingcar.view;
-
-// 사용자 입력 처리
-public class InputView {
-}
